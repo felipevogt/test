b423e77de45c881aeeea0cd65cc8f2e297c9375e
felipevogtf <felipevogt.f@gmail.com>
2021-07-26 21:40:17 -0400

    asd

--- index.php
+++ index.php
@@ -8,6 +8,12 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
 $request = $_SERVER['REQUEST_URI'];
 
+if (!empty($_POST)) {
+                    $data = $_POST;
+                } else {
+                    $data = json_decode(file_get_contents('php://input'), true);
+                }
+echo json_encode($data);
 $router = new Router();
 
 $router->routes($request);
